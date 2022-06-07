6f2a18246c380e7f0fef584e9f9d503ddfd318a5
David Lopez <sebastian.830@outlook.com>
2022-06-07 13:37:31 -0500

    Login Completado

--- index.py
+++ index.py
@@ -1,3 +1,4 @@
+from csv import excel_tab
 from flask import Flask, render_template, request, redirect, url_for
 
 
@@ -11,26 +12,53 @@ usuarios_Registro = []
 def home():
     return render_template('home.html')
 
+
 # Ruta para el login
 @app.route('/Login', methods=['GET','POST'])
 def Login():
+    #Obtiene los datos del pagina Login por medio del metodo POST
+    if(request.method == "POST"):
+        correo = request.form['email']          #obtencion de correo
+        contrase = request.form['password']         #obtencion de contraseña
+
+
+        #try para evaluar si un correo no se encuentra en la lista
+        try:
+            if (usuarios_Registro.index(correo) > 0):               #obtener el indice del correo
+                posicionCorreo = usuarios_Registro.index(correo)
+                if(usuarios_Registro[posicionCorreo] == correo and usuarios_Registro[posicionCorreo+1] == contrase):        #compara los datos ingresados con el registro
+                    return redirect(url_for('Vuelos'))              #envia a la pagina Vuelos
+                else:
+                    return redirect(url_for('Login'))
+        except:
+            return redirect(url_for('Login'))
+
     return render_template('Login.html')
+
 
 # Ruta para el registro
 @app.route('/Registro' , methods=['GET','POST'])
 def Registro():
+     #Obtiene los datos del pagina Login por medio del metodo POST
     if(request.method == "POST"):
-        nombre = request.form['name']
-        correo = request.form['email']
+        nombre = request.form['name']           #Registro datos usuario
+        correo = request.form['email']         
         contrase = request.form['password']
-
-        if(nombre == "" or correo  == "" or contrase == ""):
+        if(nombre == "" or correo  == "" or contrase == ""):             #Si no hay datos no redirecciona
             return redirect(url_for('Registro'))
         else:
-            usuarios_Registro.append({'nombre': nombre, 'email': correo, 'password': contrase})
-            print(usuarios_Registro)
+            #Ingresa datos en el array del Usuario
+            usuarios_Registro.append(nombre)
+            usuarios_Registro.append(correo)
+            usuarios_Registro.append(contrase)
             return redirect(url_for('Login'))
     return render_template('Registro.html')
+
+
+@app.route('/Vuelos')
+def Vuelos():
+    
+    return render_template('Vuelos.html')
 
 
 # Ejecutar la aplicacion
